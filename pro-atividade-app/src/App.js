8eea956c045476f2833f69b4860070bd62e04536
vinicius <vmm_7@hotmail.com>
2023-02-28 22:48:24 -0300

    limpeza dos arquivos e instalações

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const atividades = [
+    {
+      id: 1,
+      descricao: "atividade",
+    },
+    {
+      id: 2,
+      descricao: "atividade",
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li>
+            {atividades[0].id} {atividades[0].descricao}
+          </li>
+          <li onClick={() => console.log("olá!")}>
+            {atividades[1].id} {atividades[1].descricao}
+          </li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
