80ad3c8c2c7ef25a7d7ae9784d87b0825cb78b08
vinicius <vmm_7@hotmail.com>
2023-03-20 23:00:13 -0300

    Início do 'form' em App.js

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -13,26 +13,34 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <ul>
-          <li>
-            {atividades[0].id} {atividades[0].descricao}
-          </li>
-          <li onClick={() => console.log("olá!")}>
-            {atividades[1].id} {atividades[1].descricao}
-          </li>
-        </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input id="id" type="text" placeholder="id" />
+        <input id="descricao" type="text" placeholder="descrição" />
+        <button>+ Atividade</button>
+      </form>
+      <div className="mt-3">
+        <header className="list-group">
+          <ul>
+            {atividades.map(atividade => {
+              return (
+                <li key={atividade.id} className="list-group-item">
+                  {atividade.id} - {atividade.descricao}
+                </li>
+              );
+            })}
+          </ul>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
