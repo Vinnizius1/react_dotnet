d73b026f83037a61adf5ad16967523201a9ae676
vinicius <vmm_7@hotmail.com>
2023-03-26 19:43:50 -0300

    Evoluindo Form

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -22,6 +22,8 @@ function App() {
     e.preventDefault();
     const atividade = {
       id: document.getElementById("id").value,
+      prioridade: document.getElementById("prioridade").value,
+      titulo: document.getElementById("titulo").value,
       descricao: document.getElementById("descricao").value,
     };
     setAtividades([...atividades, { ...atividade }]);
@@ -31,13 +33,30 @@ function App() {
     <>
       <form className="row g-3 m-2">
         <div className="col-md-6">
-          <label for="id" className="form-label">
+          <label htmlFor="id" className="form-label">
             Id
           </label>
           <input id="id" type="text" className="form-control" />
         </div>
         <div className="col-md-6">
-          <label for="descricao" className="form-label">
+          <label htmlFor="inputState" className="form-label">
+            Prioridade
+          </label>
+          <select id="prioridade" className="form-select">
+            <option defaultValue="0">Selecionar...</option>
+            <option value="1">Baixa</option>
+            <option value="2">Normal</option>
+            <option value="3">Alta</option>
+          </select>
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="titulo" className="form-label">
+            Título
+          </label>
+          <input id="titulo" type="text" className="form-control" />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="descricao" className="form-label">
             Descrição
           </label>
           <input id="descricao" type="text" className="form-control" />
@@ -60,12 +79,12 @@ function App() {
                       <span className="badge text-bg-secondary me-1">
                         {atividade.id}
                       </span>
-                      - título
+                      {atividade.titulo}
                     </h5>
                     <h6 style={h6style}>
                       Prioridade:
                       <img src={faceSmile} alt={faceSmile} style={facestyle} />
-                      Normal
+                      {atividade.prioridade}
                     </h6>
                   </div>
                   <p className="card-text">{atividade.descricao}</p>
